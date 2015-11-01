aad71a5fe03e75121c7d586b576ff5a6382f50a2
Rochelle Terman <rterman@gmail.com>
2015-11-01 14:59:06 -0800

    lots of cleaning

---
+++ 09_APIs/A2_plot_duke.R
@@ -0,0 +1,21 @@
+# set working directory
+setwd(dir="~/Dropbox/berkeley/Dissertation/Data\ and\ Analyais/Git\ Repos/PS239T/09_APIs")
+
+# read CSV
+
+duke.articles <- read.csv("allduke.csv")
+names(duke.articles)
+
+# Add year-month indicators
+duke.articles$year.month<-format(as.Date(duke.articles$date), "%Y-%m")
+duke.articles$year.month<-as.Date(paste0(duke.articles$year.month, "-01"))
+
+# Count articles per month
+library(plyr)
+duke.permonth<-ddply(.data = duke.articles, .variables = "year.month", summarize, count=length(year.month))
+
+# Plot the trend over time
+library(ggplot2)
+ggplot(data = duke.permonth, aes(x = year.month, y = count))+geom_point()+geom_smooth(se=F)+
+  theme_bw()+xlab(label = "Date")+ylab(label = "Article Count")+ggtitle(label = "Coverage of Duke Ellington")
+```
